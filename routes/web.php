518868fdb2ad6a4079b811af9f380002ffd9d547
Rodolfo Zabala <rodolfo.zabala@grupoacin.local>
2018-11-20 17:32:40 +0000

    Update

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,6 @@ Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about' );
 Route::get('/services', 'PagesController@services');
 
+route::resource('posts', 'PostsController');
+
 
