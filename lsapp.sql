6901d4877ba3f440badc51675aec10c57ac228a0
Rodolfo Zabala <rodolfo.zabala@grupoacin.local>
2018-11-22 12:48:43 +0000

    Database updated

--- lsapp.sql
+++ lsapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 20-Nov-2018 às 17:54
+-- Generation Time: 22-Nov-2018 às 13:47
 -- Versão do servidor: 10.1.33-MariaDB
 -- PHP Version: 7.2.6
 
@@ -41,7 +41,9 @@ CREATE TABLE `migrations` (
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2018_11_20_163753_create_posts_table', 1);
+(3, '2018_11_20_163753_create_posts_table', 1),
+(4, '2018_11_21_164438_add_user_id_to_posts', 2),
+(5, '2018_11_22_103211_add_cover_image_to_posts', 3);
 
 -- --------------------------------------------------------
 
@@ -66,8 +68,19 @@ CREATE TABLE `posts` (
   `title` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `body` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `cover_image` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Extraindo dados da tabela `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `body`, `created_at`, `updated_at`, `user_id`, `cover_image`) VALUES
+(14, 'Post One', '<p>This is the post one</p>', '2018-11-22 12:34:46', '2018-11-22 12:35:04', 1, 'python_1542890104.jfif'),
+(15, 'Post Two', '<p>This is the post two</p>', '2018-11-22 12:42:53', '2018-11-22 12:42:53', 1, 'noimage.jpg'),
+(16, 'Post three', '<p>This is my post three</p>', '2018-11-22 12:45:10', '2018-11-22 12:45:10', 1, 'no_image.jpg');
 
 -- --------------------------------------------------------
 
@@ -85,6 +98,14 @@ CREATE TABLE `users` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Extraindo dados da tabela `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Rodolfo', 'rodolfosifontes1@gmail.com', NULL, '$2y$10$5s7uzDKJJavnJEwa5IJ.q.1TgsksxM.tqPaliXxhEWZ/3IwlvP5h.', 'B7ev3bH8utx54JCmZ3uHIu95xm0NP2KAYHQhJcU2qQzUsBxlikoMUFrYYqLY', '2018-11-21 16:11:54', '2018-11-21 16:11:54'),
+(2, 'jhon doe', 'jhondoe@gmail.com', NULL, '$2y$10$1nqvtUvM/hxxduooUh8cgemOqWO61m5iaYgfygQN6L5B8A8l7.Xb2', 'kriZ1T26b8Bvm1wgTompRxioLOyFXrc8gM1GxuS8q7DRRYFRM24g7mJrfA5e', '2018-11-22 09:08:30', '2018-11-22 09:08:30');
 
 --
 -- Indexes for dumped tables
@@ -123,19 +144,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
